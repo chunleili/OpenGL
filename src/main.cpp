c1f024eddd23b41392c674ce9ce1eee73f0e71df
chunleili <li_cl@foxmail.com>
2022-12-28 15:28:10 +0800

    createShader

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,101 @@
 const GLuint WIDTH = 1000, HEIGHT = 800;
 
 // 键盘输入回调函数
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
     // 当用户按下ESC键,我们设置window窗口的WindowShouldClose属性为true
     // 关闭应用程序
     if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+void checkCompile(GLuint shader)
+{
+    //检查编译是否成功
+    GLint success;  //用于存储编译是否成功
+    GLchar infoLog[512];    //用于存储错误信息
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+}
+
+GLuint createShader()
+{
+    // Shaders
+    const GLchar* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 position;\n"
+    "void main()\n"
+    "{\n"
+    "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+    "}\0";
+    const GLchar* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 color;\n"
+    "void main()\n"
+    "{\n"
+    "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
+    //创建顶点着色器
+    GLuint vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    //顶点着色器源码
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    //检查编译是否成功
+    checkCompile(vertexShader);
+
+    //创建片段着色器
+    GLuint fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    //检查编译是否成功
+    checkCompile(fragmentShader);
+
+    //创建着色器程序
+    GLuint shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    //将着色器附加到着色器程序上，并链接着色器程序
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    //检查链接是否成功
+    GLint success;  //用于存储编译是否成功
+    GLchar infoLog[512];    //用于存储错误信息
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) 
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::LINK_FAILED\n\n" << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+
+
+void draw_triangle()
+{
+    //顶点数据, 三角形
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f};
+
+    //创建顶点缓冲对象
+    GLuint VBO;
+    glGenBuffers(1, &VBO);   
+    //绑定顶点缓冲对象
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);  
+    //将顶点数据复制到顶点缓冲对象中
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
 }
 
 int main()
@@ -39,7 +128,7 @@ int main()
     glfwMakeContextCurrent(window);
 
     //注册键盘输入回调函数
-    glfwSetKeyCallback(window, key_callback);  
+    glfwSetKeyCallback(window, keyCallback);  
 
     //初始化GLEW, 告知其使用modern mode
     glewExperimental = GL_TRUE;
@@ -54,6 +143,11 @@ int main()
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
 
+    GLuint shaderProgram =  createShader();
+
+    //绘制三角形
+    draw_triangle();
+
     //渲染循环
     while(!glfwWindowShouldClose(window))
     {
@@ -62,6 +156,8 @@ int main()
         //清屏
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
 
         glfwSwapBuffers(window);
     }
