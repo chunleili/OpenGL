f145285195d9ab9e6f68c297c595c5e6c8f7043d
chunleili <li_cl@foxmail.com>
2022-12-28 14:22:12 +0800

    你好窗口

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,54 @@
-// int main()
-// {
-//     return 0;
-// }
-
+#include <iostream>
+// GLEW中包含了OpenGL的头文件, 因此要放在glfw的前面
+// #define GLEW_STATIC //由于已经在cmake中定义了, 所以这里不需要再定义了
 #include <GL/glew.h>
+// GLFW
 #include <GLFW/glfw3.h>
 
-int main(void)
+// Window dimensions
+const GLuint WIDTH = 1000, HEIGHT = 800;
+
+int main()
 {
-    GLFWwindow* window;
+    //初始化GLFW
+    std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    //创建GLFW窗口对象window
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    //初始化GLEW, 告知其使用modern mode
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
     }
 
+    //设置视口
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    //渲染循环
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    //释放资源
     glfwTerminate();
     return 0;
 }
